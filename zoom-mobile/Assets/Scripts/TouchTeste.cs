4a2aebdb421c118694c0be392d8e215967c959c2
matsalen <k.kurosaki@hotmail.com>
2019-08-02 17:19:01 -0300

    Zoom no mobile com pinça

---
+++ zoom-mobile/Assets/Scripts/TouchTeste.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchTeste : MonoBehaviour
+{
+    [SerializeField] int frequencia;
+    Touch[] toques;
+    Touch toque;
+    float tempo;
+    int aux, delta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tempo = 0;
+        delta = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempo += Time.deltaTime;
+        aux = Mathf.FloorToInt(tempo);
+        if (aux > delta)
+        {
+            delta = aux + frequencia;
+            toques = Input.touches;
+
+            foreach(Touch t in toques)
+            {
+                Debug.Log("ID: " + t.fingerId + " (" + t.position.x + ", " + t.position.y);
+            }
+        }
+    }
+}
